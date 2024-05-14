d31ffc1187330751506fba1769c6ce95e57ef465
walter.duo <walter.du@outlook.com>
2024-05-14 12:42:23 +0800

    Update gateway_linux.go
    For debian if called by a no-root user

--- gateway_linux.go
+++ gateway_linux.go
@@ -11,7 +11,13 @@ func discoverGatewayOSSpecificIPv4() (ip net.IP, err error) {
 	ipstr := execCmd("sh", "-c", "route -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'")
 	ipv4 := net.ParseIP(ipstr)
 	if ipv4 == nil {
-		return nil, errCantParse
+		// For debian if called by a no-root user
+		ipstr = execCmd("sh", "-c", "/usr/sbin/route -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'")
+		ipv4 = net.ParseIP(ipstr)
+		if ipv4 == nil {
+			return nil, errCantParse
+		}
+		//return nil, errCantParse
 	}
 	return ipv4, nil
 }
